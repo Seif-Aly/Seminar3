fac3fe7a3bd7f2e9e8608e1f1b773216066450ec
seif <saly@edu.hse.ru>
2023-10-06 20:40:04 +0300

    Initial Commit

---
+++ Seminar3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar3
+//
+//  Created by seif on 2023-10-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
